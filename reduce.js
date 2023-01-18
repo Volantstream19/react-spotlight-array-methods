151c4ff714a624f0db0500af47e04afd5892eeb2
King Kong <king-kong@gmail.com>
2023-01-18 10:28:06 -0800

    reduce.js all tests passing

--- reduce.js
+++ reduce.js
@@ -2,9 +2,16 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  return brands.reduce((index, brand) => {
+    index.push(...brand.shoes);
+    return index;
+  }, []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  return dogs.reduce((total, dog) => total + dog.age, 0) / dogs.length;
+};
